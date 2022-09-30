578d44161d570d6669ae2d45516d5cbdad6d14c9
yang <joey674901205@gmail.com>
2022-09-29 22:59:11 -0400

    connect mongodb

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -52,8 +52,8 @@ const SearchBooks = () => {
       const bookData = items.map((book) => ({
         bookId: book.id,
         authors: book.volumeInfo.authors || ["No author to display"],
-        title: book.volumeInfo.title,
-        description: book.volumeInfo.description,
+        title: book.volumeInfo.title || "No title",
+        description: book.volumeInfo.description || "No description",
         image: book.volumeInfo.imageLinks?.thumbnail || "",
       }));
 
--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb+srv://longyang_1:Wly19941103@cluster0.il4yyqe.mongodb.net/googlebookdb1?retryWrites=true&w=majority" ||
+    "mongodb://localhost/googlebooks",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
